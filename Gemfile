e3a7aac1055ee12592c67e2f70ad09fffb550319
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-02-16 19:22:32 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', require: 'sinatra/base'
+gem 'sqlite3'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+
+group :development, :test do
+  gem 'shotgun'
+  gem 'minitest'
+  gem 'tux'
+end
---
+++ config/database.rb
@@ -0,0 +1,9 @@
+# set the database based on the current environment
+database_name = "task-manager-#{TaskManager.environment}"
+db = URI.parse(ENV['DATABASE_URL'] || "sqlite3://localhost/db/#{database_name}")
+
+# connect ActiveRecord with the current database
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme,
+  :database => "#{database_name}",
+)
